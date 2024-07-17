2c4aab11d694849e35507a7eb4def2eb3202b327
NicolaBellini <Bellininicola4366@gmail.com>
2024-07-17 18:06:28 +0200

    [add] bullseye with tailwind

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,7 +1,17 @@
-<template>ciaos main</template>
-
 <script>
 export default {};
 </script>
+
+<template>
+  <div class="min-h-screen grid place-content-center radial-blu">
+    <div
+      class="grid place-content-center bg-emerald-500 w-52 h-52 rounded-full shadow-2xl"
+    >
+      <div class="grid place-content-center bg-blue-400 w-32 h-32 rounded-full">
+        <div class="bg-red-400 w-12 h-12 rounded-full"></div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style></style>
