e25a43e6f82b99c1a97a2fb2aadca72ee4e2ea93
NicolaBellini <Bellininicola4366@gmail.com>
2024-07-17 17:37:18 +0200

    scafolding

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import mainVue from "./components/main.vue";
+export default {
+  components: {
+    mainVue,
+  },
+};
+</script>
+
+<template>
+  <mainVue />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/main.vue
@@ -0,0 +1,7 @@
+<template>ciaos main</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
